83bcefa6f6f5a044f0920d0a4da59a55ec64b284
Smit Mistry <smitmistry19@gmail.com>
2026-05-26 18:17:15 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      name: "Poppins",
+      provider: fontProviders.google(),
+      cssVariable: "--font-sans",
+      weights: [400, 500, 600],
+      formats: ["woff2"],
+    },
+  ],
+});
